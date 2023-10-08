3156dedb966f76442cb695205a63c898dc8adca2
IlyaLevchenko04 <nneo2086@gmail.com>
2023-10-08 16:25:02 +0300

    Added helper logic
    Wrote func calculateMovieTime which provides available movie time

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/header/header';
 import './styles/styles.css';
 import { Homepage } from './components/homepage/homepage';
+import { Helper } from './components/helper/helper';
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Header />}>
         <Route index element={<Homepage />} />
-        <Route path="/helper" element={<h1>Helper</h1>} />
+        <Route path="/helper" element={<Helper />} />
       </Route>
     </Routes>
   );
